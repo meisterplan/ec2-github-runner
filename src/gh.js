e1167533eeb2c9bdba579f28c7d72772f4d7604c
Jakob Nohe <jakob.nohe@meisterplan.com>
2021-11-16 11:24:52 +0100

    Use unique instance id as runner name, to prevent name clash
    Sometimes a GitHub actions job does not cleanup the self-hosted runner
    from the list of registered runners. Then a runner with a ip address
    as the name is left.
    
    A new runner that does potentially get the same IP as the previous runner
    is now no longer able to connect to the GitHub runner API as the runner
    is already registered (but offline).
    
    Using the EC2 instance id does ensure that the name is somewhat unique and
    does not interfere with left over, not cleaned up offline runners.
    
    KNUTH-62626

--- src/gh.js
+++ src/gh.js
@@ -3,18 +3,18 @@ const github = require('@actions/github');
 const _ = require('lodash');
 const config = require('./config');
 
-const INITIAL_WAIT_TIME_BEFORE_CHECKS = 5 * 1000; // 5 seconds
+const INITIAL_WAIT_TIME_BEFORE_CHECKS = 4 * 1000; // 5 seconds
 const MAX_WAIT_TIME_BEFORE_FAIL = 15 * 60 * 1000; // 15 minutes
 const MAX_WAIT_TIME_BETWEEN_REGISTERED_CHECKS = 60 * 1000; // 60 seconds
 
 // use the unique label to find the runner
 // as we don't have the runner's id, it's not possible to get it in any other way
-async function getRunner(label) {
+async function getRunner(runnerId) {
   const octokit = github.getOctokit(config.input.githubToken);
 
   try {
     const runners = await octokit.paginate('GET /repos/{owner}/{repo}/actions/runners', config.githubContext);
-    const foundRunners = _.filter(runners, { labels: [{ name: label }] });
+    const foundRunners = _.filter(runners, (runner) => runner.name === runnerId);
     return foundRunners.length > 0 ? foundRunners[0] : null;
   } catch (error) {
     return null;
@@ -36,12 +36,12 @@ async function getRegistrationToken() {
 }
 
 async function removeRunner() {
-  const runner = await getRunner(config.input.label);
+  const runner = await getRunner(config.input.ec2InstanceId ? config.input.ec2InstanceId : config.input.label);
   const octokit = github.getOctokit(config.input.githubToken);
 
   // skip the runner removal process if the runner is not found
   if (!runner) {
-    core.info(`GitHub self-hosted runner with label ${config.input.label} is not found, so the removal is skipped`);
+    core.info(`GitHub self-hosted runner with id ${config.input.label} is not found, so the removal is skipped`);
     return;
   }
 
@@ -55,10 +55,10 @@ async function removeRunner() {
   }
 }
 
-function waitForRunnerRegisteredRecursive(label, resolve, reject, nextDelay, timeoutAfter) {
+function waitForRunnerRegisteredRecursive(runnerId, resolve, reject, nextDelay, timeoutAfter) {
   core.info(`Waiting ${nextDelay / 1000} seconds...`);
   setTimeout(async () => {
-    const runner = await getRunner(label);
+    const runner = await getRunner(runnerId);
     if (runner && runner.status === 'online') {
       core.info(`GitHub self-hosted runner ${runner.name} is registered and ready to use`);
       resolve();
@@ -67,17 +67,18 @@ function waitForRunnerRegisteredRecursive(label, resolve, reject, nextDelay, tim
         core.error('GitHub self-hosted runner registration error');
         reject(`Timeout exceeded: Your AWS EC2 instance was not able to register itself in GitHub as a new self-hosted runner.`);
       } else {
-        waitForRunnerRegisteredRecursive(label, resolve, reject, Math.min(nextDelay * 2, MAX_WAIT_TIME_BETWEEN_REGISTERED_CHECKS), timeoutAfter);
+        const waitFor = Math.min(nextDelay * 1.5, MAX_WAIT_TIME_BETWEEN_REGISTERED_CHECKS);
+        waitForRunnerRegisteredRecursive(runnerId, resolve, reject, waitFor, timeoutAfter);
       }
     }
   }, nextDelay);
 }
 
-function waitForRunnerRegistered(label) {
+function waitForRunnerRegistered(runnerId) {
   core.info(`Checking if the GitHub self-hosted runner is registered, waiting up to ${Math.round(MAX_WAIT_TIME_BEFORE_FAIL / 1000 / 60)} minutes.`);
 
   return new Promise((resolve, reject) => {
-    waitForRunnerRegisteredRecursive(label, resolve, reject, INITIAL_WAIT_TIME_BEFORE_CHECKS, new Date().getTime() + MAX_WAIT_TIME_BEFORE_FAIL);
+    waitForRunnerRegisteredRecursive(runnerId, resolve, reject, INITIAL_WAIT_TIME_BEFORE_CHECKS, new Date().getTime() + MAX_WAIT_TIME_BEFORE_FAIL);
   });
 }
 
